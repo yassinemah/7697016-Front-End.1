4dfab194a32b39452e1528ef68377ed2c0f5431a
yassinemah <126077431+yassinemah@users.noreply.github.com>
2023-06-09 15:00:01 +0200

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -11,9 +11,15 @@ const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = `${article.disponibilite} ? "En stock" : "Rupture de stock"`;
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement);
